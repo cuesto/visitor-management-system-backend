aaca6fca4c3a5e34570a9a76df4296bbb23dd45e
Jonathan Cuesto <34cuesto@gmail.com>
2019-08-03 08:00:26 -0400

    Department Validator Added

---
+++ VMS.DataModel.Core/Validators/DepartmentValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using LinqKit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VMS.DataModel.Core.DAL;
+using VMS.DataModel.Core.Entities;
+
+namespace VMS.DataModel.Core.Validators
+{
+    public class DepartmentValidator : AbstractValidator<Department>
+    {
+        private readonly UnitOfWork _uow;
+
+        public DepartmentValidator(UnitOfWork uow)
+        {
+            _uow = uow;
+            RuleFor(department => department.Description)
+                .Must(ValidateDescription)
+                .WithMessage("Este Departamento ha sido registrado.");
+        }
+
+        private bool ValidateDescription(Department department, string description)
+        {
+            var predicate = PredicateBuilder.New<Department>();
+
+            predicate = predicate.And(c => c.DepartmentKey != department.DepartmentKey);
+            predicate = predicate.And(c => c.Description.Trim().ToLower() == description.Trim().ToLower() || c.IsDeleted != Enums.IsDeleted.False);
+
+            return _uow.GetGenericRepository<Department>().GetCount(predicate) == 0;
+
+        }
+    }
+}
