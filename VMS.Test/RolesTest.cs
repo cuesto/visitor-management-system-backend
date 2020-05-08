5a3064082631d5f65095defd563ab85e475b07c6
Jonathan Cuesto <34cuesto@gmail.com>
2020-05-07 21:47:04 -0400

    data driven test like added

---
+++ VMS.Test/RolesTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VMS.Web.Controllers;
+
+namespace VMS.Test
+{
+    [TestClass]
+    public class RolesTest
+    {
+        private Mock<RolesController> rolesControllerMock;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            rolesControllerMock = new Mock<RolesController>();
+
+        }
+    }
+}
--- VMS.Test/ServiceTest.cs
+++ VMS.Test/ServiceTest.cs
@@ -6,13 +6,15 @@ namespace VMS.Test
     [TestClass]
     public class ServiceTest
     {
-        [TestMethod]
-        public void ValidateRNCOrCedulaOnDGII()
+        [DataTestMethod]
+        [DataRow("40220097683", "JHONATAN LOAMMY CUESTO GUILLEN")]
+        [DataRow("131222872", "INFOSOCIAL SRL")]
+        public void ValidateRNCOrCedulaOnDGII(string rnc, string name)
         {
-            string nombre = "JHONATAN LOAMMY CUESTO GUILLEN";
-            var rnc = DGII.ConsultarRNC("40220097683");
+            //string nombre = "JHONATAN LOAMMY CUESTO GUILLEN";
+            var result = DGII.ConsultarRNC(rnc);
 
-            Assert.AreEqual(rnc.Nombre, nombre);
+            Assert.AreEqual(result.Nombre, name);
         }
     }
 }
