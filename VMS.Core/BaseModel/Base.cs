22e8eea4dbd2aa2f4e79abbad4f977d7effe3373
Jonathan Cuesto <34cuesto@gmail.com>
2022-04-11 10:18:39 -0400

    context added

---
+++ VMS.Core/BaseModel/Base.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace VMS.Core.BaseModel
+{
+
+    public interface IBase
+    {
+        int Id { get; set; }
+        bool Deleted { get; set; }
+    }
+
+    public class Base : IBase
+    {
+        public virtual int Id { get; set; }
+        [DefaultValue(false)]
+        public virtual bool Deleted { get; set; }
+    }
+
+
+}
---
+++ VMS.Model/Contexts/BaseDbContext.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using VMS.Core.BaseModel;
+using VMS.Model.Extensions;
+
+namespace VMS.Model.Contexts
+{
+    public interface IDbContext
+    {
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+        EntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+    }
+
+    public abstract class BaseDbContext : DbContext, IDbContext
+    {
+        public BaseDbContext(DbContextOptions options) : base(options) { }
+
+        private void SetAuditEntities() { }
+
+        public override int SaveChanges()
+        {
+            SetAuditEntities();
+            return base.SaveChanges();
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            SetAuditEntities();
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            SetAuditEntities();
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            foreach(var type in modelBuilder.Model.GetEntityTypes())
+            {
+                if (typeof(IBase).IsAssignableFrom(type.ClrType))
+                    modelBuilder.SetSoftDeleteFilter(type.ClrType);
+            }
+        }
+
+    }
+}
